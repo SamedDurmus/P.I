ce708c969ee961ed052c0ee1c40ebb9d54d3801d
Samed Durmuş <smtdrms_10@hotmail.com>
2021-06-05 21:41:57 +0300

    Update PIQ8.sql

--- PIQ8.sql
+++ PIQ8.sql
@@ -1,5 +1,5 @@
 /****** Script for SelectTopNRows command from SSMS  ******/
 SELECT  [Device_Type]
       ,REPLACE( REPLACE( REPLACE( REPLACE( [Stats_Access_Link],'https://',''),'http://',''),'<url>',''),'/</url>','') as [Stats_Access_Link]
-  FROM [NewsSiteDB].[dbo].[xmler]
+  FROM [NewsSiteDB].[dbo].[PIQ8]
 
